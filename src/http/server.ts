123d3206f60f60eee7c31937fd53cf2200b18767
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2023-02-09 16:44:03 -0300

    feat(express): adding router

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 
+import router from './router';
+
 const application = express();
+
+application.use('/api/v1', router);
 
 application.listen(8000, () => {
   console.log('Application running on port 8000');
