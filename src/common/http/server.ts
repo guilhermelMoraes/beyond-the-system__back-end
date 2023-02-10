155881bffdcaf2cd4b47d63222c2e1c2c2187ea9
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2023-02-10 17:25:09 -0300

    feat: accepting json body in http requests

--- src/common/http/server.ts
+++ src/common/http/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import router from './router';
+import v1Router from './router';
 
 function createHttpServer() {
   const application = express();
 
+  application.use(express.json());
   application.use('/api/v1', v1Router);
 
   application.listen(8000, () => {
