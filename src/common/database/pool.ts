6f73194ee7202eee5cb7e1226f07e71ca28ed270
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2023-02-10 13:11:13 -0300

    chore: updating main.ts for conditional http server bootstrapping

--- src/common/database/pool.ts
+++ src/common/database/pool.ts
@@ -1,19 +1,22 @@
 import { Pool } from 'pg';
 
-async function openDbConnection() {
-  try {
-    const pool = new Pool({
-      port: 5432,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-    });
+const pool = new Pool({
+  port: 5432,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+});
 
+async function openDbConnection(): Promise<boolean> {
+  try {
     await pool.connect();
     console.log('Database pool successfully open');
+    return true;
   } catch (error) {
     console.log(error);
+    return false;
   }
 }
 
 export default openDbConnection;
+export { pool };
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+/* eslint-disable import/first */
 import dotenv from 'dotenv';
-import openDbConnection from './common/database/pool';
-import createHttpServer from './common/http/server';
 
 dotenv.config();
 
+// Attention: no imports should go before this line. Otherwise, the .env variables will not be set
+
+import openDbConnection from './common/database/pool';
+import createHttpServer from './common/http/server';
+
 (async () => {
-  await openDbConnection();
-  createHttpServer();
+  if (await openDbConnection()) {
+    createHttpServer();
+  }
 })();
