e013129cada99307b6de1621398189eebf6fa102
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2023-02-10 09:14:01 -0300

    refactor(server): abstracting module into function

--- src/common/http/server.ts
+++ src/common/http/server.ts
@@ -2,10 +2,14 @@ import express from 'express';
 
 import router from './router';
 
-const application = express();
+function createHttpServer() {
+  const application = express();
 
-application.use('/api/v1', router);
+  application.use('/api/v1', router);
 
-application.listen(8000, () => {
-  console.log('Application running on port 8000');
-});
+  application.listen(8000, () => {
+    console.log('Application running on port 8000');
+  });
+}
+
+export default createHttpServer;
