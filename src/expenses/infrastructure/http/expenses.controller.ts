515f5783b147a168a3fe2aca30b69a81b40a9b69
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2023-02-11 17:50:11 -0300

    feat(expenses): adding service to fetch addresses based on zip code

--- src/expenses/infrastructure/http/expenses.controller.ts
+++ src/expenses/infrastructure/http/expenses.controller.ts
@@ -7,6 +7,7 @@ import { pool } from '../../../common/database/pool';
 import HttpStatusCode from '../../../common/http/status-code';
 import validationMiddleware from '../../../common/http/validation-middleware';
 import { newExpenseValidationSchema } from '../../domain/dtos/new-expense.dto';
+import ExpensesService from '../../services/expenses.service';
 import ExpensesPostgresRepository from '../database/expenses-pg.repository';
 
 const v1ExpensesController = Router();
@@ -20,6 +21,18 @@ v1ExpensesController.post(
   async (req: Request, res: Response): Promise<void> => {
     await expensesRepository.newExpense(req.body);
     res.sendStatus(HttpStatusCode.CREATED);
+  },
+);
+
+v1ExpensesController.get(
+  BASE_PATH,
+  async (req: Request, res: Response): Promise<void> => {
+    const expensesService = new ExpensesService(expensesRepository);
+    const result = await expensesService.listExpensesWithAddress();
+    res.json({
+      success: true,
+      data: result,
+    });
   },
 );
 
---
+++ src/expenses/services/expenses.service.ts
@@ -0,0 +1,51 @@
+/* eslint-disable camelcase */
+import axios from 'axios';
+
+import ExpenseProperties, { Address } from '../domain/expense';
+import { RawDbExpense } from '../infrastructure/database/expenses-pg.repository';
+import ExpensesRepository from '../repositories/expenses.interface';
+
+class ExpensesService {
+  private readonly _expensesRepository: ExpensesRepository;
+
+  constructor(expensesRepository: ExpensesRepository) {
+    this._expensesRepository = expensesRepository;
+  }
+
+  private static async mapZipCodeToAddress(zipCode: string, number: number): Promise<Address> {
+    const { data } = await axios.get<Record<string, string>>(`https://viacep.com.br/ws/${zipCode}/json/`);
+    return {
+      city: data.localidade,
+      neighborhood: data.bairro,
+      zipCode: data.cep,
+      state: data.uf,
+      streetAvenueName: data.logradouro,
+      number,
+    };
+  }
+
+  public async listExpensesWithAddress(): Promise<ExpenseProperties[]> {
+    const expenses: RawDbExpense[] = await this._expensesRepository.listCurrentMonthExpenses();
+
+    const expensesWithAddress = expenses.map(async (rawExpense): Promise<ExpenseProperties> => ({
+      id: rawExpense.id,
+      price: rawExpense.price,
+      category: {
+        id: rawExpense.category_id,
+        description: rawExpense.category_description,
+        name: rawExpense.category,
+      },
+      date: rawExpense.date ?? rawExpense.created_at,
+      description: rawExpense.description,
+      payment: rawExpense.payment,
+      address: await ExpensesService.mapZipCodeToAddress(
+        rawExpense.zip_code,
+        rawExpense.commerce_address_number,
+      ),
+    }));
+
+    return Promise.all(expensesWithAddress);
+  }
+}
+
+export default ExpensesService;
