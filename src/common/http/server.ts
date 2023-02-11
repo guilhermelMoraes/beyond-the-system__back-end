e6e000bf26b712445778f00fedcb68f7f084eb20
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2023-02-11 19:18:04 -0300

    feat: adding cors

--- src/common/http/server.ts
+++ src/common/http/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 import v1Router from './router';
 
 function createHttpServer() {
   const application = express();
 
+  application.use(cors());
   application.use(express.json());
   application.use('/api/v1', v1Router);
 
